1c8abb979374667b5d15a1de6f239ddca1f4dce0
jayson_kesterson <your_email@example.com>
2020-10-03 23:53:11 -0400

    adding second prop for Url to row and importing Row

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row.js';
+import requests from './requests.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Lets build netflix twin</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
