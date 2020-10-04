3dc0673c8c3566f772392e377cce37eda99f1efc
jayson_kesterson <your_email@example.com>
2020-10-03 23:54:03 -0400

    using useEffect to fetch data for movies, and setting it in state

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "./axios.js";
+import "./Row.css"
 
-function Row() {
-    return(
-        <div>
 
-        </div>
-    )
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchReq = await axios.get(fetchUrl);
+      setMovies(fetchReq.data.results);
+      return fetchReq;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__images">
+        {movies.map((movie) => (
+          <img
+            className="row__image"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
