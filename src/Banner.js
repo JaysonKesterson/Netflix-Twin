cd01e5438642ec193dfd132dfb542f3688f2acca
jayson_kesterson <your_email@example.com>
2020-10-12 10:32:06 -0400

    adding truncation

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios.js'
 import requests from './requests.js'
+import './Banner.css'
 
 function Banner() {
     const [movie, setMovie]=useState([]);
@@ -17,6 +18,10 @@ function Banner() {
         fetchData();
     }, []);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    }
+
     return(
         <header className="banner" 
         style={{
@@ -28,17 +33,19 @@ function Banner() {
     }}
         >
             <div className="banner__contents">
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
 
                 <div className="banner__buttons">
-                    <button className="banner_button">Play</button>
-                    <button className="banner_button">My List</button>
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
                 </div>
 
-                <h1 className="banner__description">{movie?.overview}</h1>
+                <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
             </div>
+
+            <div className="banner--fadeBottom" />
         </header>
     )
 }
