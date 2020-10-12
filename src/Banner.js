00a0291f6569448e78f4af607c485e838a8d7721
jayson_kesterson <your_email@example.com>
2020-10-12 09:42:55 -0400

    asychronously getting movie into state for banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from './axios.js'
+import requests from './requests.js'
 
 function Banner() {
+    const [movie, setMovie]=useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[Math.floor(Math.random() * request.data.results.length-1)
+                ]
+        );
+        return request;
+        }
+        fetchData();
+    }, []);
+
     return(
         <header>
-
+            <div>
+                
+            </div>
+            {/* div > 2 buttons */}
         </header>
     )
 }
